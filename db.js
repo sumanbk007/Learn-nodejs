86712844a81af0943afaaaaa325926e0040bcd66
Sumanbk007 <bksuman1211@gmail.com>
2024-04-13 13:25:59 +0545

    Configure dotEnv

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 //mongoDB url
 
-const mongoUrl = "mongodb://127.0.0.1:27017/mydatabase"; //You can replace mydatabase with your database name.
-
+const mongoUrl = process.env.mongoDbUrl;
+// "mongodb://127.0.0.1:27017/mydatabase";
 //set up mongoDB Connections
 
 mongoose.connect(mongoUrl, {
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to node js learning");
@@ -19,6 +22,6 @@ const menuListRoutes = require("./routes/menuListRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuListRoutes);
 
-app.listen(3000, () => {
-  console.log(`Server running on port 3000`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
